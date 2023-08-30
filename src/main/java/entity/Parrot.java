9e39f6cc30c22c376b570f0e2bf840f665a54c23
osparking <jbpark03@gmail.com>
2023-08-30 23:09:21 +0900

    print count of parrot created so far

--- src/main/java/entity/Parrot.java
+++ src/main/java/entity/Parrot.java
@@ -5,10 +5,16 @@ import lombok.Data;
 @Data
 public class Parrot {
 	private String name;
+	private Integer count = 0;
 
 	@Override
 	public String toString() {
 		return "Parrot [name=" + name + "]";
+	}
+
+	public Parrot() {
+		super();
+		System.out.println("Parrot count: " + ++count);
 	}
 	
 }
